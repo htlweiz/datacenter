3e74065ae81b733a84e9c563aab698fcb3ab5a7c
DavidTorbics20 <torbic190220@sr.htlweiz.at>
2022-12-16 11:50:41 +0100

    made some changes

--- datacenter/model/__init__.py
+++ datacenter/model/__init__.py
@@ -3,8 +3,10 @@
 
 from .base import Base
 from .foo_bar import FooBar
+from .address import Address
 from .email import Email
 from .room import Room
+from .device_type import DeviceType
 from .network_usage import NetworkUsage
 
 __exports__ = [
