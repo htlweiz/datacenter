7540bc433320bce55d02c199e85274eb8daf00da
MaNameJonas <jonas.glieder@gmail.com>
2022-12-16 12:05:38 +0100

    resolve merge issues

--- datacenter/model/__init__.py
+++ datacenter/model/__init__.py
@@ -3,27 +3,22 @@
 
 from .base import Base
 from .foo_bar import FooBar
-<<<<<<< HEAD
 from .UserRole import UserRole
-=======
 from .email import Email
 from .room import Room
 from .address import Address
 from .socket import Socket
 from .network_usage import NetworkUsage
->>>>>>> c0cfe62dad01969f87ce39bbbfb97ffb176c6b43
+from .device_type import DeviceType
 
 __exports__ = [
     Base,
     FooBar,
-<<<<<<< HEAD
-    UserRole
-=======
+    UserRole,
     Email,
     Address,
     Room,
     Socket,
     DeviceType,
     NetworkUsage,
->>>>>>> c0cfe62dad01969f87ce39bbbfb97ffb176c6b43
 ]
