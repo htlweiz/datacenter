b4bd0431fc3ef94e8587957a37e81032dc6ae273
Matthias Serschön <sersch190092@sr.htlweiz.at>
2022-12-16 11:44:32 +0100

    created floor.py

---
+++ datacenter/model/floor.py
@@ -0,0 +1,11 @@
+import sqlalchemy
+
+class Floor:
+    __tablename__ = "floor"
+    floorID = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    floor_name = sqlalchemy.Column(sqlalchemy.VARCHAR, nullable=False)
+    building_ID = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(building_ID))
+
+    def __repr__(self):
+        """Generate nice representation!"""
+        return "Floor:%s" %self.floor_name
