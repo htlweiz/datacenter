1a94cd78d480ad131344e0f5b9332fe521f83602
Matthias Serschön <sersch190092@sr.htlweiz.at>
2022-12-16 12:13:53 +0100

    finite 2.OOO

--- datacenter/model/floor.py
+++ datacenter/model/floor.py
@@ -1,10 +1,11 @@
 import sqlalchemy
+from .building import Building
 
 class Floor:
     __tablename__ = "floor"
     floorID = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     floor_name = sqlalchemy.Column(sqlalchemy.VARCHAR, nullable=False)
-    building_ID = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('building_ID'))
+    building_ID = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('building_id'))
 
     def __repr__(self):
         """Generate nice representation!"""
